fec55545a3fd5a1cc3f6abdee09ceab5da282b26
Aaron Huus <aaron.huus@target.com>
2019-03-21 20:31:36 -0500

    basic CRUD working on books

--- phrase_book/api/books.py
+++ phrase_book/api/books.py
@@ -10,6 +10,7 @@ from phrase_book.settings import DISPLAY_NAME
 books_blueprint = Blueprint('books', __name__)
 
 
+# Create a Book
 @books_blueprint.route('/', methods=['POST'])
 def create_books():
     body = json.loads(request.data)
@@ -19,19 +20,37 @@ def create_books():
     return jsonify(book), 201
 
 
+# Get All Books
 @books_blueprint.route('/', methods=['GET'])
 def books():
     all_books = Book.query.all()
     return jsonify(all_books)
 
 
+# Get one Book
 @books_blueprint.route('/<book_id>', methods=['GET'])
 def get_book(book_id: str):
-    book = get_book_by_id(book_id)
+    book = Book.query.get(book_id)
     if not book:
         raise NotFound(f'Book with id: {book_id} was not found')
     return jsonify(book)
 
 
-def get_book_by_id(book_id: str):
-    return Book()
+# Update a Book
+@books_blueprint.route('/<book_id>', methods=['PUT'])
+def update_a_book(book_id: str):
+    body = json.loads(request.data)
+    db.session.query(Book).filter(Book.id == book_id).update(body)
+    db.session.commit()
+    return jsonify(Book.query.get(book_id))
+
+
+# Delete a Book
+@books_blueprint.route('/<book_id>', methods=['DELETE'])
+def delete_a_book(book_id: str):
+    book = Book.query.get(book_id)
+    if not book:
+        raise NotFound(f'Book with id: {book_id} was not found')
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({'status': 'ok'})
--- phrase_book/api/phrases.py
+++ phrase_book/api/phrases.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, jsonify
 from werkzeug.exceptions import NotFound
 
-from phrase_book.api.books import get_book_by_id
 from phrase_book.models.book import Book
 
 phrases_blueprint = Blueprint('phrases', __name__)
@@ -9,13 +8,13 @@ phrases_blueprint = Blueprint('phrases', __name__)
 
 @phrases_blueprint.route('/phrases', methods=['GET'])
 def phrases(book_id):
-    book: Book = get_book_by_id(book_id)
+    book: Book = Book('a')
     return jsonify(book.phrases)
 
 
 @phrases_blueprint.route('/phrases/<phrase_id>', methods=['GET'])
 def get_phrase(book_id, phrase_id):
-    book: Book = get_book_by_id(book_id)
+    book: Book = Book('b')
     filtered_phrases = [p for p in book.phrases if p.id == phrase_id]
     if not filtered_phrases:
         raise NotFound(f'Phrase with id: {phrase_id} of book with id: {book_id} was not found')
